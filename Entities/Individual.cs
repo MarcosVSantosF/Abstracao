5438f680c1c79b402d16ca4f16311d8c3f4794d3
Marcos Vinicius <marcosvsantosf@gmail.com>
2023-12-27 18:40:13 -0300

    First Commit

---
+++ Entities/Individual.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex_Proposto_2;
+
+public class Individual : TaxPayer
+{
+    public double HealthExpenditures {get; set;}
+
+    public Individual()
+    {
+    }
+    public Individual(string name, double anualIncome, double healthExpenditures) : base(name, anualIncome)
+    {
+        HealthExpenditures = healthExpenditures;
+    }
+
+    public override double Tax()
+    {
+        if(AnualIncome < 20000.00)
+        {
+            return AnualIncome * 0.15 - HealthExpenditures * 0.5;
+        }
+        else
+        {
+            return AnualIncome * 0.25 - HealthExpenditures * 0.5;
+        }
+
+               
+    }
+}
---
+++ Entities/TaxPayer.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex_Proposto_2;
+
+public abstract class TaxPayer
+{
+    public string Name { get; set; }
+    public double AnualIncome { get; set; }
+
+    public TaxPayer()
+    {
+    }
+
+    public TaxPayer(string name, double anualIncome)
+    {
+        Name = name;
+        AnualIncome = anualIncome;
+    }
+
+    public abstract double Tax();
+
+    
+}
